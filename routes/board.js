b6ad274f556bed4f2ab2e4ac8ac0953e768ecd67
Sounwoo <mna800@naver.com>
2022-06-15 15:56:24 +0900

    조회 수정본

--- routes/board.js
+++ routes/board.js
@@ -37,7 +37,7 @@ router.post('/api/travels', postController.createPost);
 // ========================================================================================
 
 // // 게시물 조회
-router.get('/api/travels', postController.getPost);
+router.get('/api/travel', postController.getPost);
 
 //=============================================================================================
 
